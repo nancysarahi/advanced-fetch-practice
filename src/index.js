ea9f59420f879a95ddc7b156a5c22978b1635924
Nancy Montiel <nancysarahi@Nancys-MacBook-Pro.local>
2017-08-23 19:05:15 -0500

    fetched messages and comments

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 
+// callback function
 function usersRetrieved(response) {
   return response.json();
 }
+
 function userJSONReady(users) {
   const usersDiv = document.getElementById("users");
   let usersHTML = "";
@@ -12,3 +14,33 @@ function userJSONReady(users) {
   usersDiv.innerHTML = (usersHTML);
 }
 
+// let promise = fetch("https://jsonplaceholder.typicode.com/users");
+// let jsonpromise = promise.then(usersRetrieved);
+// jsonpromise.then(userJSONReady);
+
+fetch("https://jsonplaceholder.typicode.com/users")
+.then((response)=> response.json())
+.then(userJSONReady);
+
+
+function commentsRetrieved(response) {
+  return response.json();
+}
+
+function commentsJSONReady(comments) {
+  const commentsDiv = document.getElementById("comments");
+  let commentsHTML = "";
+  for (let i = 0; i < comments.length; i++) {
+    const comment = comments[i];
+    commentsHTML += "<div>" + comment.name + "</div>";
+  }
+  commentsDiv.innerHTML = (commentsHTML);
+}
+
+fetch("https://jsonplaceholder.typicode.com/comments")
+.then((response)=> response.json())
+.then(commentsJSONReady);
+
+// let promise = fetch("https://jsonplaceholder.typicode.com/comments");
+// let jsonpromise = promise.then(commentsRetrieved);
+// jsonpromise.then(commentsJSONReady);
